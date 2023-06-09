567ebde9e8d439603c7b7bb2e4bc4602d895d510
NaveedIlyas <naveedilyas321@gmail.com>
2023-06-09 12:57:47 +0500

    Update json-server.js

--- json-server.js
+++ json-server.js
@@ -1,12 +1,16 @@
-import pkg from 'json-server';
-const { create, router: _router, defaults } = pkg;
-const server = create()
-const router = _router('db.json')
-const middlewares = defaults()
-const port=process.env.PORT||3000
+import { create, router as _router, defaults } from 'json-server';
 
-server.use(middlewares)
-server.use(router)
+const server = create();
+const router = _router('db.json');
+const middlewares = defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use('/users', router); // Use '/users' as the base URL for the router
+server.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the JSON server' });
+});
+
 server.listen(port, () => {
-  console.log('JSON Server is running on '+port)
-})
+  console.log('JSON Server is running on ' + port);
+});
