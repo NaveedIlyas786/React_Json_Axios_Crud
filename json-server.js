34ff3fa2bb2cdcb483cc86824013dea41d82dcc6
NaveedIlyas <naveedilyas321@gmail.com>
2023-06-09 13:05:58 +0500

    Update json-server.js

--- json-server.js
+++ json-server.js
@@ -1,11 +1,13 @@
-import pkg from 'json-server';
-const { create, router: _router, defaults } = pkg
+import * as pkg from 'json-server';
+
+const { create, router: _router, defaults } = pkg;
+const server = create();
 const router = _router('db.json');
 const middlewares = defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use('/users', router); // Use '/users' as the base URL for the router
+server.use('/users', router);
 server.get('/', (req, res) => {
   res.json({ message: 'Welcome to the JSON server' });
 });
