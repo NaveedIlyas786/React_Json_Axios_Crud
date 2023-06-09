317153eb36a8bbd48fcfc39bca694ef1d666fd79
NaveedIlyas <naveedilyas321@gmail.com>
2023-06-09 12:42:58 +0500

    Update json-server.js

--- json-server.js
+++ json-server.js
@@ -1,4 +1,5 @@
-import { create, router as _router, defaults } from 'json-server'
+import pkg from 'json-server';
+const { create, router: _router, defaults } = pkg;
 const server = create()
 const router = _router('db.json')
 const middlewares = defaults()
